8fa77b44a4afb04adc058fbcb12134cb2840715b
Sebastien Gatty <tyganit@gmail.com>
2018-02-08 17:32:41 +0100

    Moving object script

---
+++ Assets/Scripts/MovingObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingObject : MonoBehaviour
+{	
+	public Vector2 Displacement = Vector2.up;
+	public float Period = 1f;
+	public float Phase = 0f;
+	private Vector3 _initialPosition;
+
+	private void Start ()
+	{
+		_initialPosition = transform.position;
+	}
+
+	private void Update ()
+	{
+		transform.position = Vector3.Lerp(_initialPosition, _initialPosition + (Vector3) Displacement, Mathf.PingPong(Time.time + Phase, Period * 0.5f) / (Period * 0.5f));
+	}
+}
