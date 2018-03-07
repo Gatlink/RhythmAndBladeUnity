a95987f39e74d2c2209811c74e263cc364639976
Sebastien Andary <sebastien.andary@gmail.com>
2018-03-07 14:01:20 +0100

    add death timer zones

---
+++ Assets/Scripts/DeathTimer.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Linq;
+using UnityEngine;
+
+public class DeathTimer : MonoBehaviour
+{
+    public delegate void TickHandler( bool dealsDamage );
+
+    public event TickHandler TickEvent;
+    
+    public float TimeoutTicks = 10;
+    public int DamagePerTick = 1;
+    public float TickPeriodSeconds = 3;
+
+    private Mobile _player;
+    private Collider2D[] _zoneColliders;
+
+    private IEnumerator Start()
+    {
+        _player = GameObject.FindGameObjectWithTag( Tags.Player ).GetComponent<Mobile>();
+        _zoneColliders = GetComponentsInChildren<Collider2D>();
+
+        yield return new WaitUntil( PlayerInZone );
+
+        for ( int i = 0; i < TimeoutTicks; i++ )
+        {
+            if ( PlayerInZone() )
+            {
+                OnTickEvent( false );
+            }
+            yield return new WaitForSeconds( TickPeriodSeconds );
+        }
+
+        // deal damage every ticks when player is in zone
+        while ( true )
+        {
+            if ( PlayerInZone() )
+            {                
+                _player.GetComponent<ActorHealth>().AccountDamages( DamagePerTick, gameObject );
+                OnTickEvent( true );
+            }
+            yield return new WaitForSeconds( TickPeriodSeconds );
+        }
+    }
+
+    private bool PlayerInZone()
+    {
+        return _zoneColliders.Any( col => col.OverlapPoint( _player.BodyPosition ) );
+    }
+
+    private void OnTickEvent( bool dealsdamage )
+    {
+        var handler = TickEvent;
+        if ( handler != null ) handler( dealsdamage );
+    }
+}
---
+++ Assets/Scripts/DeathTimerEffect.cs
@@ -0,0 +1,51 @@
+﻿using Gamelogic.Extensions;
+using UnityEngine;
+
+public class DeathTimerEffect : GLMonoBehaviour
+{
+    public float FadeDuration = 0.4f;
+
+    [ Range( 0, 1 ) ]
+    public float EffectAlpha = 0.5f;
+
+    private Texture2D _vignetteTexture;
+
+    private void Awake()
+    {
+        _vignetteTexture = Resources.Load<Texture2D>( "Vignette" );
+    }
+
+    private void OnEnable()
+    {
+        GetRequiredComponent<DeathTimer>().TickEvent += TickEventHandler;
+    }
+
+    private void OnDisable()
+    {
+        var deathTimer = GetRequiredComponent<DeathTimer>();
+        if ( deathTimer != null )
+        {
+            deathTimer.TickEvent -= TickEventHandler;
+        }
+    }
+
+    private void TickEventHandler( bool dealsDamage )
+    {
+        var cameraFade = CameraFade.Instance;
+
+        var texture = cameraFade.FadeTexture;
+        var color = cameraFade.FadeColor;
+
+        cameraFade.FadeColor = dealsDamage ? Color.red : Color.white;
+        cameraFade.FadeTexture = _vignetteTexture;
+        CameraFade.FadeFrom( EffectAlpha, FadeDuration, () =>
+        {
+            cameraFade.FadeColor = color;
+            cameraFade.FadeTexture = texture;
+        } );
+        if ( dealsDamage )
+        {
+            CameraShake.ScreenShake( 1, FadeDuration );
+        }
+    }
+}
