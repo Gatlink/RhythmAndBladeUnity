a48005796d2c13dc67d091d38e0cd427023037a1
Sebastien Andary <sebastien.andary@gmail.com>
2018-03-09 21:47:39 +0100

    adds beat manager (event source)

---
+++ Assets/Scripts/BeatManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using Gamelogic.Extensions;
+using UnityEngine;
+
+public class BeatManager : Singleton<BeatManager>
+{
+    public delegate void BeatEventHandler( int beatCount, float nextBeat );
+
+    public event BeatEventHandler BeatEvent;
+
+    public float FirstBeatOffset;
+
+    public float BeatPerMinute;
+
+    public float BeatPeriodSeconds
+    {
+        get { return 60 / BeatPerMinute; }
+    }
+
+    public float Time
+    {
+        get { return _source.time; }
+    }
+
+    private AudioSource _source;
+
+    private void Awake()
+    {
+        _source = GetRequiredComponent<AudioSource>();
+    }
+
+    private void OnEnable()
+    {
+        StartCoroutine( PlayCoroutine() );
+    }
+
+    private void OnDisable()
+    {
+        _source.Stop();
+        StopAllCoroutines();
+    }
+
+    private IEnumerator PlayCoroutine()
+    {
+        _source.Play();
+
+        yield return new WaitUntil( () => Time >= FirstBeatOffset );
+
+        int beatCount = 0;
+        var targetTime = FirstBeatOffset;
+        var totalTime = _source.clip.length;
+        while ( targetTime <= totalTime )
+        {
+            targetTime += BeatPeriodSeconds;
+            var nextBeat = targetTime - Time;
+            OnBeatEvent( beatCount, nextBeat );
+            yield return new WaitForSecondsRealtime( nextBeat );
+        }
+    }
+
+    private void OnBeatEvent( int beatCount, float nextBeat )
+    {
+        var handler = BeatEvent;
+        if ( handler != null ) handler( beatCount, nextBeat );
+    }
+}
