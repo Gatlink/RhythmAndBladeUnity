1c930c36ced27bcfb1136a22ccbdc084aa142d65
Sebastien Gatty <tyganit@gmail.com>
2018-03-19 18:21:07 +0100

    Big LD changes

---
+++ Assets/Scripts/Placeholders/ShrinkOnHit.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShrinkOnHit : MonoBehaviour
+{
+	public float Factor = 0.5f;
+	
+	private Transform _parent;
+
+	private void OnEnable()
+	{
+		var destroyable = GetComponentInParent<DestroyWhenHit>();
+		if ( destroyable != null )
+		{
+			_parent = destroyable.transform;
+			destroyable.HitEvent += OnHit;
+		}
+	}
+
+	private void OnDisable()
+	{
+		var destroyable = GetComponentInParent<DestroyWhenHit>();
+		if ( destroyable != null )
+		{
+			destroyable.HitEvent -= OnHit;
+		}
+	}
+
+	private void OnHit()
+	{
+		var scale = _parent.localScale;
+		scale.x = Factor * scale.x;
+		_parent.localScale = scale;
+	}
+}
