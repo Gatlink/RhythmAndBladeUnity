e3bfa7129652c30a5e917cbbd39966ea7c6e6f85
Sebastien Andary <sebastien.andary@gmail.com>
2018-03-25 21:22:18 +0200

    adds standard script field in boss behaviour custom editor

--- Assets/Scripts/Controllers/Editor/BossBehaviourEditor.cs
+++ Assets/Scripts/Controllers/Editor/BossBehaviourEditor.cs
@@ -8,6 +8,11 @@ public class BossBehaviourEditor : Editor
 {
     public override void OnInspectorGUI()
     {
+        GUI.enabled = false;
+        EditorGUILayout.ObjectField( "Script:", MonoScript.FromScriptableObject( (BossBehaviour) target ),
+            typeof( BossBehaviour ), false );
+        GUI.enabled = true;
+
         var bossBehaviour = (BossBehaviour) target;
         GUILayout.Label( string.Format( "{0} node(s)", bossBehaviour.TotalNodeCount ) );
         if ( GUILayout.Button( "View in Editor" ) )
