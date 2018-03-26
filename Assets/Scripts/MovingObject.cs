14a7e48e659d824b6072bf652f387d46aeb07f70
Sebastien Andary <sebastien.andary@gmail.com>
2018-03-26 16:17:17 +0200

    adds repeat loop mode to moving objects

--- Assets/Scripts/MovingObject.cs
+++ Assets/Scripts/MovingObject.cs
@@ -1,16 +1,28 @@
 ﻿using System;
 using UnityEngine;
+using UnityEngine.Serialization;
 #if UNITY_EDITOR
 using UnityEditor;
 
 #endif
 
-[DefaultExecutionOrder(-101)]
+[ DefaultExecutionOrder( -101 ) ]
 public class MovingObject : MonoBehaviour, IMoving
 {
+    public enum LoopType
+    {
+        PingPong,
+        Repeat
+    }
+
     public Vector2 Displacement = Vector2.up;
-    public float Period = 1f;
+
+    public float LoopPeriod;
+
     public float Phase;
+
+    public LoopType Loop;
+
     private Vector2 _initialPosition;
 
     public bool Enabled
@@ -22,8 +34,14 @@ public class MovingObject : MonoBehaviour, IMoving
     {
         get
         {
-            return Displacement / ( Period / 2 ) *
-                   Mathf.Sign( 0.5f - Mathf.Repeat( Time.time + Phase, Period ) / Period );
+            var slope = Displacement / LoopPeriod;
+            float sign = 1;
+            if ( Loop == LoopType.PingPong )
+            {
+                sign = Mathf.Sign( 0.5f - Mathf.Repeat( Time.time + Phase, 2 * LoopPeriod ) / ( 2 * LoopPeriod ) );
+            }
+
+            return sign * slope;
         }
         set { throw new NotSupportedException( "Cannot set current velocity" ); }
     }
@@ -41,8 +59,19 @@ public class MovingObject : MonoBehaviour, IMoving
 
     private void Update()
     {
-        transform.position = Vector2.Lerp( _initialPosition, _initialPosition + Displacement,
-            Mathf.PingPong( Time.time + Phase, Period * 0.5f ) / ( Period * 0.5f ) );
+        switch ( Loop )
+        {
+            case LoopType.PingPong:
+                transform.position = Vector2.Lerp( _initialPosition, _initialPosition + Displacement,
+                    Mathf.PingPong( Time.time + Phase, LoopPeriod ) / LoopPeriod );
+                break;
+            case LoopType.Repeat:
+                transform.position = Vector2.Lerp( _initialPosition, _initialPosition + Displacement,
+                    Mathf.Repeat( Time.time + Phase, LoopPeriod ) / LoopPeriod );
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
     }
 
 #if UNITY_EDITOR
